74b4ce63060bc574ce005fd2775ad4ef32697e49
Blatalzar <bataprod@gmail.com>
2024-09-18 09:49:41 +0200

    Display initial drawing

--- app/javascript/controllers/drawing_controller.js
+++ app/javascript/controllers/drawing_controller.js
@@ -8,6 +8,18 @@ export default class extends Controller {
     this.canvas.isDrawingMode = true
     this.canvas.freeDrawingBrush.width = 5
     this.canvas.freeDrawingBrush.color = "#000000"
+    this.loadExistingImage()
+  }
+
+  loadExistingImage() {
+    const existingImageData = this.imageDataTarget.value
+    if (existingImageData) {
+      fabric.Image.fromURL(existingImageData, (img) => {
+        this.canvas.clear()
+        this.canvas.add(img)
+        this.canvas.renderAll()
+      })
+    }
   }
 
   clear() {
