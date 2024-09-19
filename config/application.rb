6776d771f6e7b31874ce82d285877d3eb5ee17f4
Blatalzar <bataprod@gmail.com>
2024-09-19 13:31:29 +0200

    Better endgame screen

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ module Japongo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
 
+    # Allow requests from ngrok
+    config.hosts << "fa73-2a02-842a-8204-d201-2937-5008-f3c2-b988.ngrok-free.app"
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
