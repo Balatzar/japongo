284a76556f26b2d8373611b29b13044e3fbf7df7
Blatalzar <bataprod@gmail.com>
2024-09-19 23:47:18 +0200

    Fix missing chars

--- app/services/crossword_game_initializer_service.rb
+++ app/services/crossword_game_initializer_service.rb
@@ -6,17 +6,27 @@ class CrosswordGameInitializerService
 
   def self.initialize_game
     @@all_words = Word.all.index_by(&:romanji)
-    words = @@all_words.values.sample(50)
+    words = @@all_words.values.sample(400)
     biggest_word = words.max_by { |word| word.romanji.length }
+    pp "biggest_word: #{biggest_word.romanji.inspect}"
     grid = initialize_grid
     placed_words = [ biggest_word ]
     place_biggest_word(grid, biggest_word)
+    pp grid
+
+    maximum = 50
+    i = 0
 
     while placed_words.size < WORDS_TO_PLACE
       word_to_place = find_intersecting_word(words - placed_words, placed_words)
+      pp "word_to_place: #{word_to_place.romanji.inspect}"
       break unless word_to_place
+      break if i > maximum
+      i += 1
 
       if place_word(grid, word_to_place, placed_words, words)
+        pp "Word has been placed"
+        pp grid
         placed_words << word_to_place
       end
     end
@@ -48,7 +58,9 @@ class CrosswordGameInitializerService
   def self.check_words_in_line(line)
     line.split(" ").each do |word_candidate|
       next if word_candidate.length <= 1
+      pp @@all_words
       unless @@all_words.key?(word_candidate)
+        pp "Word not found: #{word_candidate.inspect}"
         return false
       end
     end
@@ -79,30 +91,35 @@ class CrosswordGameInitializerService
   def self.place_word(grid, word, placed_words, words)
     placed_chars = placed_words.flat_map { |w| w.romanji.chars }.uniq
     common_chars = word.romanji.chars & placed_chars
+    pp "common_chars: #{common_chars.inspect}"
 
     common_chars.each do |common_char|
       word_index = word.romanji.index(common_char)
+      pp "word_index: #{word_index.inspect}"
 
       grid.length.times do |row|
         grid.length.times do |col|
           if grid[row][col] == common_char
+            pp "common_char: #{common_char.inspect}"
+            pp "row: #{row.inspect}, col: #{col.inspect}"
             if can_place_horizontally?(grid, word, row, col - word_index)
               place_horizontally(grid, word, row, col - word_index)
               if check_grid(grid)
                 return true
               else
-                remove_horizontally(grid, word, row, col - word_index)
+                remove_horizontally(grid, word, row, col - word_index, col)
                 words.delete(word)
-                return false
               end
             elsif can_place_vertically?(grid, word, row - word_index, col)
+              pp "We can place vertically"
               place_vertically(grid, word, row - word_index, col)
               if check_grid(grid)
                 return true
               else
-                remove_vertically(grid, word, row - word_index, col)
+                pp "The grid was invalid"
+                pp grid
+                remove_vertically(grid, word, row - word_index, col, row)
                 words.delete(word)
-                return false
               end
             end
           end
@@ -147,15 +164,15 @@ class CrosswordGameInitializerService
     end
   end
 
-  def self.remove_horizontally(grid, word, row, start_col)
+  def self.remove_horizontally(grid, word, row, start_col, initial_col)
     word.romanji.length.times do |index|
-      grid[row][start_col + index] = " "
+      grid[row][start_col + index] = " " unless start_col + index == initial_col # Don't remove the intersecting character
     end
   end
 
-  def self.remove_vertically(grid, word, start_row, col)
+  def self.remove_vertically(grid, word, start_row, col, initial_row)
     word.romanji.length.times do |index|
-      grid[start_row + index][col] = " "
+      grid[start_row + index][col] = " " unless start_row + index == initial_row # Don't remove the intersecting character
     end
   end
 end
